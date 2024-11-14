4933af17ec074738186cc025758285fdc978dfbb
Anna Alimanova <ax1243bb@gmail.com>
2024-11-14 09:27:58 +0300

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Anna on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
